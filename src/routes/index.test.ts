2a7080cb15c0124cc0afef58e5afea5c89f75065
timsonner <timsonner@gmail.com>
2022-01-09 06:54:18 +0000

    Add import

--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -1,4 +1,6 @@
-import { render } from "@testing-library/svelte";
+import { render } from "@testing-library/svelte"
+// extend-expect needs to be imported for .toBeInTheDocument()
+import '@testing-library/jest-dom/extend-expect'
 import Index from './index.svelte'
 
 describe("Test if Jest is working", () => {
