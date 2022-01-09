855e4dad4f630755e7e0d7f98249f7b96dcf243a
timsonner <timsonner@gmail.com>
2022-01-09 07:38:01 +0000

    Jest config update

--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -1,11 +1,11 @@
 import { render } from "@testing-library/svelte"
 // extend-expect needs to be imported for .toBeInTheDocument()
-import '@testing-library/jest-dom/extend-expect'
+// import '@testing-library/jest-dom/extend-expect'
 import Index from './index.svelte'
 
 describe("Test if Jest is working", () => {
 	test('Welcome', () => {
-		const { getByText } = render(Index);
+		const { getByText } = render(Index)
 		expect(getByText('Welcome to SvelteKit')).toBeInTheDocument()
 	});
 });
