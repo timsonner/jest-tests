66051950b1df48d6379eeeddc0506d46f5ec6a36
timsonner <timsonner@gmail.com>
2022-01-09 20:16:24 +0000

    Added new test

--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/svelte"
 // extend-expect needs to be imported for .toBeInTheDocument()
 // import '@testing-library/jest-dom/extend-expect'
+// now imported from ../../testSetupFile.ts
+
+import { render, screen } from "@testing-library/svelte"
 import Index from './index.svelte'
 
 describe("Test if Jest is working", () => {
@@ -9,3 +11,11 @@ describe("Test if Jest is working", () => {
 		expect(getByText('Welcome to SvelteKit')).toBeInTheDocument()
 	});
 });
+
+describe("Query by title", () => {
+	test("Does query have a title?", () => {
+		render(Index)
+		const node = screen.getByTitle('title')
+		expect(node).toBeInTheDocument()
+	})
+})
